89e7d6672eb9d3fb009cad4015f1c91e32603163
Matthew Pickin <matthewpickin@nas10-240-170-239.york.ac.uk>
2018-07-06 14:16:56 +0100

    Added the initial files & manifest directories

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# Class installs ntp and makes sure it's running. Self contained in
+# a class so that it's super simple to run.
+
+class pbg_ntp {
+	# Install the ntp package 
+	ensure_packages(['ntp']
+
+	# Ensures config is correct before service can run
+	file { '/etc/ntp.conf':
+		source  => 'puppet://modules/pbg_ntp/ntp.conf',
+		notify  => Service['ntp']
+		require => Package['ntp']
+	}
+
+	# Service only runs if file is present.
+	service { 'ntp':
+		ensure => running,
+		enable => true,
+	}
+}
+
