6677c9a68ecf9a5cdd0c34b9e7bf860d75a1aa75
Matthew Pickin <matthewpickin@nas10-240-170-239.york.ac.uk>
2018-07-06 15:21:07 +0100

    Fixed some tabs in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,20 +2,20 @@
 # a class so that it's super simple to run.
 
 class pbg_ntp {
-	# Install the ntp package 
-	ensure_packages(['ntp']
+  # Install the ntp package 
+  ensure_packages(['ntp']
 
-	# Ensures config is correct before service can run
-	file { '/etc/ntp.conf':
-		source  => 'puppet://modules/pbg_ntp/ntp.conf',
-		notify  => Service['ntp']
-		require => Package['ntp']
-	}
+  # Ensures config is correct before service can run
+  file { '/etc/ntp.conf':
+    source  => 'puppet://modules/pbg_ntp/ntp.conf',
+    notify  => Service['ntp']
+    require => Package['ntp']
+  }
 
-	# Service only runs if file is present.
-	service { 'ntp':
-		ensure => running,
-		enable => true,
-	}
+  # Service only runs if file is present.
+  service { 'ntp':
+    ensure => running,
+    enable => true,
+  }
 }
 
